a7f3ca9f44b2066c8a8468c4740d977c1eb731f0
Jason <872272528@qq.com>
2021-02-24 12:01:45 +0800

    递归算法提交

--- LeetCode/LeetCodeQuestions/LeetCode70_ClimbSteps.swift
+++ LeetCode/LeetCodeQuestions/LeetCode70_ClimbSteps.swift
@@ -37,6 +37,26 @@
 import UIKit
 
 class LeetCode70_ClimbSteps: NSObject {
+    /*
+     递归思想简单
+     但是不可取
+     */
+//    func climbStairs(_ n: Int) -> Int {
+//        if(n == 0){
+//            return 0
+//        }
+//
+//        if(n == 1){
+//            return 1
+//        }
+//
+//        if( n == 2){
+//            return 2
+//        }
+//
+//        return climbStairs(n - 1) + climbStairs(n - 2)
+//    }
+    
     func climbStairs(_ n: Int) -> Int {
         if(n == 0){
             return 0
